e556fe0af0f3c92407ed511dc899432ac5f4df93
Arnold Gandarillas Castillo <arkgast@gmail.com>
2021-11-20 18:08:14 -0400

    chore: add to do list

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,20 +6,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Winlo = await ethers.getContractFactory("Winlo");
+  const winlo = await Winlo.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await winlo.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Winlo deployed to:", winlo.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
